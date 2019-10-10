e6e61e88eb84738d2e5c77acd17aeccf0c8b3bd8
Jarmo Kortetjärvi <jarmo.kortetjarvi@gmail.com>
2019-10-10 20:09:21 +0300

    [ADD] Basic logic for matching invoices via invoice reference

--- account_bank_statement_reconcile_reference/models/account_bank_statement_line.py
+++ account_bank_statement_reconcile_reference/models/account_bank_statement_line.py
@@ -3,7 +3,6 @@ from odoo import models, api
 
 
 class AccountBankStatementLine(models.Model):
-
     _inherit = "account.bank.statement.line"
 
     def get_reconciliation_proposition(self, excluded_ids=None):
@@ -13,13 +12,18 @@ class AccountBankStatementLine(models.Model):
         # Try to match via payment reference
         if self.ref:
             # Match invoice
-            invoice = self.env('account.invoice').search([
+            invoice = self.env['account.invoice'].search([
                 ('ref_number', '=', self.ref)
             ], limit=1)
 
-            if invoice:
-                self.env['account.move.line'].search([
-                    # Conditions
-                ], limit=1)
+            if invoice and invoice.move_id:
+                # An invoice is found, and it has an account move
+
+                # Filter moves that are for receivable accounts
+                moves = invoice.move_id.line_ids.filtered(
+                    lambda r: r.account_id.user_type_id.type == 'receivable')
+
+                if moves:
+                    res = moves[0]
 
         return res
