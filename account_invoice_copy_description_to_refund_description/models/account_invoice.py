f2b9a2172d6fc171fcefc3d83a7a97b6b2e80bde
difbit <timo.kekalainen@hotmail.com>
2019-11-04 12:47:06 +0200

    [ADD] Python file

---
+++ account_invoice_copy_description_to_refund_description/models/account_invoice.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+from odoo import api, models
+
+
+class AccountInvoice(models.Model):
+
+    _inherit = 'account.invoice'
+
+    @api.model
+    def _prepare_refund(self, invoice, date_invoice=None, date=None,
+                        description=None, journal_id=None):
+
+        res = super(AccountInvoice, self)._prepare_refund(
+            invoice, date_invoice=date_invoice, date=date,
+            description=description, journal_id=journal_id,
+        )
+
+        if self.name and res.get('name'):
+            res['name'] = self.name
+        else:
+            res['name'] = ''
+
+        return res
