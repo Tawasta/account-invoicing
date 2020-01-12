171d665e7601fd5cc69c75e915735ee8f99d9e84
difbit <timo.kekalainen@hotmail.com>
2020-01-12 22:59:04 +0200

    [IMP] Using email only now

--- account_invoice_salesperson_phone_and_email/models/sale_order.py
+++ account_invoice_salesperson_phone_and_email/models/sale_order.py
@@ -1,5 +1,5 @@
 
-from odoo import api, models
+from odoo import api, models, _
 
 
 class SaleOrder(models.Model):
@@ -11,19 +11,14 @@ class SaleOrder(models.Model):
         """Assign phone and email values to Invoice's comment-field"""
         vals = super(SaleOrder, self)._prepare_invoice()
 
-        phone, email = (self.user_id.partner_id.phone,
-                        self.user_id.partner_id.email)
+        email = self.user_id.partner_id.email
 
         note = vals['comment']
 
         # Conditions to add newlines
-        email_line = '\n' if note or phone else ''
-        phone_line = '\n' if note else ''
+        email_line = '\n' if note else ''
 
-        vals['comment'] = "%s%s%s" % (
-            '%s' % note if note else '',
-            '%s%s' % (phone_line, phone) if phone else '',
-            '%s%s' % (email_line, email) if email else ''
-        )
+        if email:
+            vals['comment'] += '{}{}{}'.format(email_line, _("Handler: "), email)
 
         return vals
