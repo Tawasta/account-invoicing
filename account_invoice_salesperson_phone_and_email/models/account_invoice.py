a38672d5e5b9a9773e997653ff63dc2eb2c563e8
difbit <timo.kekalainen@hotmail.com>
2020-01-12 22:58:21 +0200

    [IMP] "Handler: "-text added and only email is used now

--- account_invoice_salesperson_phone_and_email/models/account_invoice.py
+++ account_invoice_salesperson_phone_and_email/models/account_invoice.py
@@ -1,40 +1,29 @@
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
 
 
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    salesperson_phone = fields.Char()
-
     salesperson_email = fields.Char()
 
     @api.onchange('user_id')
     def user_id_onchange_phone_and_email(self):
-        phone, email = (self.user_id.partner_id.phone,
-                        self.user_id.partner_id.email)
+        email = self.user_id.partner_id.email or ''
 
-        if self.salesperson_phone:
-            self.comment = self.comment.replace(
-                '\n' + self.salesperson_phone, '')
         if self.salesperson_email:
             self.comment = self.comment.replace(
-                '\n' + self.salesperson_email, '')
+                '\n' + _("Handler: ") + self.salesperson_email, '')
+            self.comment = self.comment.replace(
+                 _("Handler: ") + self.salesperson_email, '')
 
         if self._origin.user_id:
-            old_phone, old_email = (self._origin.user_id.partner_id.phone or '',
-                                    self._origin.user_id.partner_id.email or '')
+            old_email = self._origin.user_id.partner_id.email or ''
+            self._origin.comment = self._origin.comment.replace('\n' + _("Handler: ") + old_email, '')
 
-            self.comment = self._origin.comment.replace(
-                '\n' + old_phone, '').replace('\n' + old_email, '')
+        email_line = '\n' if self.comment else ''
 
-        email_line = '\n' if self.comment or phone else ''
-        phone_line = '\n' if self.comment else ''
-
-        if phone:
-            self.comment += '{}{}'.format(phone_line, phone)
         if email:
-            self.comment += '{}{}'.format(email_line, email)
+            self.comment += '{}{}{}'.format(email_line, _("Handler: "), email)
 
-        self.salesperson_phone = self.user_id.phone
-        self.salesperson_email = self.user_id.email
+        self.salesperson_email = self.user_id.partner_id.email
